1cdcafab669c7247cb8cf29824b256bb771dca67
Jerry Liu <jerry.liu@shopify.com>
2017-01-18 11:56:48 -0500

    move compile_tests to initialize

--- liquid/benchmarks/bm_render.rb
+++ liquid/benchmarks/bm_render.rb
@@ -2,8 +2,7 @@ require_relative 'support/benchmark_liquid.rb'
 require_relative 'support/theme_runner.rb'
 
 profiler = ThemeRunner.new
-profiler.compile_tests
 
 Benchmark.liquid("render") do
-  profiler.benchmark_render
+  profiler.render
 end
